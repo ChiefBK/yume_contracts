b2cd3a20e3f16fc72c6cb9cb10aac65afa42a21c
Ian Pierce <ian.pierce@hey.com>
2021-06-26 19:30:16 -0500

    test file; ignores .idea folder

---
+++ test_this.js
@@ -0,0 +1,9 @@
+console.log(this);
+console.log(typeof this);
+console.log(Object.getOwnPropertyNames(this));
+console.log(Object.getOwnPropertyDescriptors(this));
+console.log(Object.keys(this));
+console.log(this == null);
+console.log(this === null);
+console.log(this === undefined);
+console.log(this.prototype);
