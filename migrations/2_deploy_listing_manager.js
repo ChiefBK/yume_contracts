51b5d52ce564ee01bc5a6808f77bd5a44d690784
Ian Pierce <ian.pierce@hey.com>
2021-07-08 16:45:29 -0500

    deploy and link new contracts and libraries

--- migrations/2_deploy_listing_manager.js
+++ migrations/2_deploy_listing_manager.js
@@ -1,6 +1,13 @@
 const ListingManager = artifacts.require("ListingManager");
+const Manager = artifacts.require("Manager");
+const DateTime = artifacts.require("DateTime");
 
 module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(DateTime);
+  await deployer.link(DateTime, [Manager, ListingManager]);
+  await deployer.deploy(Manager);
   await deployer.deploy(ListingManager);
+  const manager = await Manager.deployed();
   const listingManager = await ListingManager.deployed();
+  const dateTime = await DateTime.deployed();
 };
