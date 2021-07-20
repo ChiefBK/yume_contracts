752563e5ea632bf60f978dd4fa76d24367905276
Ian Pierce <ian.pierce@hey.com>
2021-07-20 00:34:19 -0500

    checks that price is equal to amount of Wei sent; added listing manager reference

--- migrations/4_deploy_booking_manager.js
+++ migrations/4_deploy_booking_manager.js
@@ -1,8 +1,11 @@
 const BookingManager = artifacts.require("BookingManager");
+const ListingManager = artifacts.require("ListingManager");
 const DateTime = artifacts.require("DateTime");
 
 module.exports = async function(deployer, network, accounts) {
+  const listingManager = await ListingManager.deployed();
+
   await deployer.link(DateTime, BookingManager);
-  await deployer.deploy(BookingManager);
+  await deployer.deploy(BookingManager, listingManager.address);
   const bookingManager = await BookingManager.deployed();
 };
--- src/contracts/BookingManager.sol
+++ src/contracts/BookingManager.sol
@@ -3,19 +3,34 @@ pragma solidity ^0.8.0;
 
 import "./structs/Booking.sol";
 import "./Manager.sol";
+import "./ListingManager.sol";
 
 contract BookingManager is Manager {
-    mapping(uint => Booking) bookings;
     uint public numOfBookings = 0;
     uint public bookingIndex = 1; // bookings start at index 1
     string public name = "Booking Manager";
+
+    mapping(uint => Booking) bookings;
+    ListingManager listingManager;
+    mapping(address => uint) public guestEscrow;
 
     // maps a given listing ID (key) to a mapping ZZ.
     // The keys of ZZ are epoch times (at midnight) which map to a booking ID (the value)
     mapping(uint => mapping(uint => uint)) listingsToBookingDates;
 
-    function createBooking(uint _listingId, uint64 _startTime, uint64 _endTime) public isAtMidnight2(_startTime, _endTime) returns (uint) {
+    constructor(ListingManager _listingManager) {
+        listingManager = _listingManager;
+    }
+
+    function createBooking(uint _listingId, uint64 _startTime, uint64 _endTime) public isAtMidnight2(_startTime, _endTime) payable returns (uint) {
         uint bookingId = bookingIndex++;
+
+        uint expectedPrice = listingManager.determinePrice(_listingId, _startTime, _endTime);
+
+        // check that the amount sent is equal to the expected price
+        require(msg.value == expectedPrice);
+
+        guestEscrow[msg.sender] += expectedPrice;
 
         Booking storage newBooking = bookings[bookingId];
         newBooking.startEpochTime = _startTime;
